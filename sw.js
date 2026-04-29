bfcf2b255917c317fd954178b61c01b7ca16a91d
alytobbala <75489694+alytobbala@users.noreply.github.com>
2026-04-29 07:22:24 +0200

    Added initial files

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'padel-squad-v1';
+const ASSETS = ['./padel-tracker.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
